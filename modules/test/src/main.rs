f54a960e3b8369607e380221188e0a6a1e7bcff1
Chloe Bethel <chloe@9net.org>
2022-10-06 19:25:07 +0100

    reorder this to not always deadlock

--- modules/test/src/main.rs
+++ modules/test/src/main.rs
@@ -9,13 +9,11 @@ use process::ipc;
 fn main() {
 	println!("Hello from test!");
 
-	let fs_handle = ipc::sm::get_service_handle(syscalls::make_tag("fs")).unwrap();
-	println!("fs handle: {:?}", fs_handle);
-
-	println!("SM IPC");
-	ipc::sm::stop();
 	println!("FS IPC");
 	ipc::fs::stop();
+	println!("SM IPC");
+	ipc::sm::stop();
+
 	println!("Done");
 
 	syscalls::exit_process();
