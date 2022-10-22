9733708a820e91d0317480b7fb6a02d5ba1ac7f6
Chloe Bethel <chloe@9net.org>
2022-10-22 03:27:04 +0100

    remove allocator (now handled by std)

--- libprocess/src/allocator.rs
+++
@@ -1,7 +0,0 @@
-use simple_chunk_allocator::{heap, heap_bitmap, GlobalChunkAllocator, PageAligned};
-static mut HEAP: PageAligned<[u8; 1048576]> = heap!();
-static mut HEAP_BITMAP: PageAligned<[u8; 512]> = heap_bitmap!();
-
-#[global_allocator]
-static ALLOCATOR: GlobalChunkAllocator =
-    unsafe { GlobalChunkAllocator::new(HEAP.deref_mut_const(), HEAP_BITMAP.deref_mut_const()) };
