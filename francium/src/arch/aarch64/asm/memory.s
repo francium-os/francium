87fc1f60f2494a54b340288e3d6a023aad896448
Chloe Bethel <chloe@9net.org>
2022-11-20 00:13:19 +0000

    move some asm to inline

--- francium/src/arch/aarch64/asm/memory.s
+++ francium/src/arch/aarch64/asm/memory.s
@@ -1,47 +1,7 @@
-.global set_ttbr0_el1
-.global set_ttbr1_el1
-.global get_sctlr_el1
-.global set_sctlr_el1
-.global get_tcr_el1
-.global set_tcr_el1
 .global wfe
 .global invalidate_tlb
 
 .section .text
-set_ttbr0_el1:
-msr ttbr0_el1, x0
-
-tlbi vmalle1
-dsb ish
-isb
-ret
-
-set_ttbr1_el1:
-msr ttbr1_el1, x0
-
-tlbi vmalle1
-dsb ish
-isb
-IC IALLUIS
-
-ret
-
-get_sctlr_el1:
-mrs x0, sctlr_el1
-ret
-
-set_sctlr_el1:
-msr sctlr_el1, x0
-ret
-
-get_tcr_el1:
-mrs x0, tcr_el1
-ret
-
-set_tcr_el1:
-msr tcr_el1, x0
-ret
-
 wfe:
 wfe
 ret
