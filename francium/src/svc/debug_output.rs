c4d9574b945bff6af9713afe958c840116a853ba
Chloe Bethel <chloe@9net.org>
2023-01-24 19:42:04 +0000

    fix overlong debug_output hanging

--- francium/src/svc/debug_output.rs
+++ francium/src/svc/debug_output.rs
@@ -4,5 +4,8 @@ pub fn svc_debug_output(user_ptr: *const u8, len: usize) {
         core::ptr::copy_nonoverlapping(user_ptr, temp_buffer.as_mut_ptr(), len);
     }
 
-    print!("{}", core::str::from_utf8(&temp_buffer[0..len]).unwrap());
+    // These are on seperate lines so a potential panic doesn't occur inside the print
+    // (which locks the serial port, leading to a hang when panic tries to print).
+    let as_utf8 = core::str::from_utf8(&temp_buffer[0..len]).unwrap();
+    print!("{}", as_utf8);
 }
