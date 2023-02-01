8b3155b40c192e6412420dfb60c59a1d8c57f8fe
Chloe Bethel <chloe@9net.org>
2023-02-01 03:16:09 +0000

    The diagram in the PCIe spec is backwards. Why.

--- modules/pcie/src/ecam.rs
+++ modules/pcie/src/ecam.rs
@@ -3,18 +3,22 @@
 #[derive(Copy, Clone, Debug)]
 #[repr(packed)]
 pub struct ECAMHeader {
-	pub device_id: u16,
 	pub vendor_id: u16,
-	pub status: u16,
+	pub device_id: u16,
+	
 	pub command: u16,
-	pub class: u8,
-	pub subclass: u8,
-	pub prog_if: u8,
+	pub status: u16,
+	
 	pub revision: u8,
-	pub bist: u8,
-	pub header_type: u8,
+
+	pub prog_if: u8,
+	pub subclass: u8,
+	pub class: u8,
+
+	pub cache_line_size: u8,
 	pub latency_timer: u8,
-	pub cache_line_size: u8
+	pub header_type: u8,
+	pub bist: u8,
 }
 
 /*struct ECAMDevice {
